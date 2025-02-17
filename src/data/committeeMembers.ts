42e9259524c3ba79c4b8a7914de35db9f8850f1c
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2025-02-17 15:12:37 +0545

    add local team photo

--- src/data/committeeMembers.ts
+++ src/data/committeeMembers.ts
@@ -1,82 +1,126 @@
-
 import UcaLogo from "@assets/logo.svg";
+import YushImage from "@assets/localteam/yush.jpg";
+import RoshanImage from "@assets/localteam/roshan.jpg";
+import RupeshImage from "@assets/localteam/rupesh.jpg";
+import BadalImage from "@assets/localteam/badal.jpg";
+import JamesImage from "@assets/localteam/james.jpg";
+import AayushImage from "@assets/localteam/aayush.jpg";
+import AadarshImage from "@assets/localteam/aadarsh.jpg";
+import RabinaImage from "@assets/localteam/rabina.jpg";
+
 export const committeeMembers = [
-    {
-        name: "Youngbin Han",
-        team: "Global team & Ubuntu Korea Community, Ubuntu LoCo Council",
-        role: "Steering, Sponsorship & Finances",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/1916739?v=4",
-    },
-    {
-        name: "Khairul Aizat Bin Kamarudzzaman",
-        team: "Global team & Ubuntu Malaysia, Ubuntu Membership Board",
-        role: "Steering, Sponsorship & Finances",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/405473?v=4",
-    },
-    {
-        name: "Muhd Syazwan",
-        team: "Global team & Ubuntu Malaysia",
-        role: "Web, Logistics & Facility, Content",
-        profileImageUrl: UcaLogo.src,
-    },
-    {
-        name: "RJ Hsiao",
-        team: "Global team & Ubuntu Taiwan",
-        role: "Content, Web",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/236116?v=4",
-    },
-    {
-        name: "Aryan Kaushik",
-        team: "Global team & Ubuntu India, GNOME Foundation member, GNOME UG Delhi-NCR",
-        role: "Content, Web, Travel support",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/34372791?v=4",
-    },
-    {
-        name: "Bhavanishankar Ravindra",
-        team: "Global team & Ubuntu India, Ubuntu LoCo Council",
-        role: "Sponsorship & Finances, Content",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/4153385?v=4",
-    },
-    {
-        name: "Saputro Aryulianto",
-        team: "Global team & Ubuntu Indonesia, OpenInfra Indonesia",
-        role: "Web, Logistics & Facility",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/8587010?v=4",
-    },
-    {
-        name: "Ronit Banerjee",
-        team: "Global team",
-        role: "Sponsorship & Finances",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/91361382?v=4"
-    },
-    {
-        name: "Aaditya Singh",
-        team: "Local team & GNOME Nepal",
-        role: "Local team chief + Steering, Sponsorship & Finances",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/142494719?v=4"
-    },
-    {
-        name: "Sailesh Singh",
-        team: "Local team & GNOME Nepal",
-        role: "Travel support",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/58818777?v=4"
-    },
-    {
-        name: "Utsav Bhattarai",
-        team: "Local team",
-        role: "Web, Content",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/95236817?v=4"
-    },
-    {
-        name: "Rabina Karki",
-        team: "Local team",
-        role: "Sponsorship & Finances",
-        profileImageUrl: UcaLogo.src,
-    },
-    {
-        name: "Aadarsha Dhakal",
-        team: "Local team",
-        role: "",
-        profileImageUrl: UcaLogo.src,
-    }
-]
+  {
+    name: "Youngbin Han",
+    team: "Global team & Ubuntu Korea Community, Ubuntu LoCo Council",
+    role: "Steering, Sponsorship & Finances",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/1916739?v=4",
+  },
+  {
+    name: "Khairul Aizat Bin Kamarudzzaman",
+    team: "Global team & Ubuntu Malaysia, Ubuntu Membership Board",
+    role: "Steering, Sponsorship & Finances",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/405473?v=4",
+  },
+  {
+    name: "Muhd Syazwan",
+    team: "Global team & Ubuntu Malaysia",
+    role: "Web, Logistics & Facility, Content",
+    profileImageUrl: UcaLogo.src,
+  },
+  {
+    name: "RJ Hsiao",
+    team: "Global team & Ubuntu Taiwan",
+    role: "Content, Web",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/236116?v=4",
+  },
+  {
+    name: "Aryan Kaushik",
+    team: "Global team & Ubuntu India, GNOME Foundation member, GNOME UG Delhi-NCR",
+    role: "Content, Web, Travel support",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/34372791?v=4",
+  },
+  {
+    name: "Bhavanishankar Ravindra",
+    team: "Global team & Ubuntu India, Ubuntu LoCo Council",
+    role: "Sponsorship & Finances, Content",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/4153385?v=4",
+  },
+  {
+    name: "Saputro Aryulianto",
+    team: "Global team & Ubuntu Indonesia, OpenInfra Indonesia",
+    role: "Web, Logistics & Facility",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/8587010?v=4",
+  },
+  {
+    name: "Ronit Banerjee",
+    team: "Global team",
+    role: "Sponsorship & Finances",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/91361382?v=4",
+  },
+  {
+    name: "Aaditya Singh",
+    team: "Local team & GNOME Nepal",
+    role: "Local team chief + Steering, Sponsorship & Finances",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/142494719?v=4",
+  },
+  {
+    name: "Sailesh Singh",
+    team: "Local team & GNOME Nepal",
+    role: "Travel support",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/58818777?v=4",
+  },
+  {
+    name: "Utsav Bhattarai",
+    team: "Local team & GNOME Nepal",
+    role: "Web, Content",
+    profileImageUrl: "https://avatars.githubusercontent.com/u/95236817?v=4",
+  },
+  {
+    name: "Rabina Karki",
+    team: "Local team",
+    role: "Sponsorship & Finances",
+    profileImageUrl: RabinaImage.src,
+  },
+  {
+    name: "Aadarsha Dhakal",
+    team: "Local team",
+    role: "Registration & People Coordination",
+    profileImageUrl: AadarshImage.src,
+  },
+  {
+    name: "Yush Pokharel",
+    team: "Local team",
+    role: "Marketing & Design, Sponsorship & Finances",
+    profileImageUrl: YushImage.src,
+  },
+  {
+    name: "Roshan Pandey",
+    team: "Local team",
+    role: "Web & Documentation, Logistics & Facility",
+    profileImageUrl: RoshanImage.src,
+  },
+  {
+    name: "Rupesh Nepal",
+    team: "Local team",
+    role: "Registration & People Coordination, Content",
+    profileImageUrl: RupeshImage.src,
+  },
+  {
+    name: "Badal Raut",
+    team: "Local team",
+    role: "Marketing & Design",
+    profileImageUrl: BadalImage.src,
+  },
+  {
+    name: "James Bhattarai",
+    team: "Local team",
+    role: "Sponsorship & Finances",
+    profileImageUrl: JamesImage.src,
+  },
+  {
+    name: "Aayush Pokharel",
+    team: "Local team",
+    role: "Marketing & Design",
+    profileImageUrl: AayushImage.src,
+  },
+];
