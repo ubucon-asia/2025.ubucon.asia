3c844d28680879d658d07cf3a7771632492b29b2
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2025-03-10 18:11:13 +0545

    docs: add media partner

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,96 +1,119 @@
 
 import logoPlaceHolder from "@assets/logo_placeholder.svg";
+import annapurna from "@assets/sponsors/annapurna.png"
 
 
 export const sponsors = [
-    {
-        sponsorLevelName: "Diamond",
-        sizeOnLargeScreen: 6,
-        sizeOnMediumScreen: 6,
-        sizeOnSmallScreen: 6,
-        showPopup: true,
-        list: [
-            {
-                name: "Become a sponsor!",
-                logoUrl: logoPlaceHolder.src,
-                url: "/sponsors/become-a-sponsor",
-                description: "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!"
-            },
-        ]
-    },
-    {
-        sponsorLevelName: "Gold",
-        sizeOnLargeScreen: 4,
-        sizeOnMediumScreen: 4,
-        sizeOnSmallScreen: 3,
-        showPopup: true,
-        list: [
-            {
-                name: "Become a sponsor!",
-                logoUrl: logoPlaceHolder.src,
-                url: "/sponsors/become-a-sponsor",
-                description: "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!"
-            },
-        ]
-    },
-    {
-        sponsorLevelName: "Silver",
-        sizeOnLargeScreen: 3,
-        sizeOnMediumScreen: 3,
-        sizeOnSmallScreen: 2,
-        showPopup: true,
-        list: [
-            {
-                name: "Become a sponsor!",
-                logoUrl: logoPlaceHolder.src,
-                url: "/sponsors/become-a-sponsor",
-                description: "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!"
-            },
-        ]
-    },
-    {
-        sponsorLevelName: "Bronze",
-        sizeOnLargeScreen: 2,
-        sizeOnMediumScreen: 2,
-        sizeOnSmallScreen: 1,
-        showPopup: true,
-        list: [
-            {
-                name: "Become a sponsor!",
-                logoUrl: logoPlaceHolder.src,
-                url: "/sponsors/become-a-sponsor",
-                description: "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!"
-            },
-        ]
-    },
-    {
-        sponsorLevelName: "Supporter",
-        sizeOnLargeScreen: 2,
-        sizeOnMediumScreen: 2,
-        sizeOnSmallScreen: 1,
-        showPopup: true,
-        list: [
-            {
-                name: "Become a sponsor!",
-                logoUrl: logoPlaceHolder.src,
-                url: "/sponsors/become-a-sponsor",
-                description: "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!"
-            },
-        ]
-    },
-    {
-        sponsorLevelName: "Community Partners",
-        sizeOnLargeScreen: 2,
-        sizeOnMediumScreen: 2,
-        sizeOnSmallScreen: 1,
-        showPopup: false,
-        list: [
-            {
-                name: "Become a sponsor!",
-                logoUrl: logoPlaceHolder.src,
-                url: "/sponsors/become-a-sponsor",
-                description: "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!"
-            },
-        ]
-    }
-]
+  {
+    sponsorLevelName: "Diamond",
+    sizeOnLargeScreen: 6,
+    sizeOnMediumScreen: 6,
+    sizeOnSmallScreen: 6,
+    showPopup: true,
+    list: [
+      {
+        name: "Become a sponsor!",
+        logoUrl: logoPlaceHolder.src,
+        url: "/sponsors/become-a-sponsor",
+        description:
+          "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      },
+    ],
+  },
+  {
+    sponsorLevelName: "Gold",
+    sizeOnLargeScreen: 4,
+    sizeOnMediumScreen: 4,
+    sizeOnSmallScreen: 3,
+    showPopup: true,
+    list: [
+      {
+        name: "Become a sponsor!",
+        logoUrl: logoPlaceHolder.src,
+        url: "/sponsors/become-a-sponsor",
+        description:
+          "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      },
+    ],
+  },
+  {
+    sponsorLevelName: "Silver",
+    sizeOnLargeScreen: 3,
+    sizeOnMediumScreen: 3,
+    sizeOnSmallScreen: 2,
+    showPopup: true,
+    list: [
+      {
+        name: "Become a sponsor!",
+        logoUrl: logoPlaceHolder.src,
+        url: "/sponsors/become-a-sponsor",
+        description:
+          "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      },
+    ],
+  },
+  {
+    sponsorLevelName: "Bronze",
+    sizeOnLargeScreen: 2,
+    sizeOnMediumScreen: 2,
+    sizeOnSmallScreen: 1,
+    showPopup: true,
+    list: [
+      {
+        name: "Become a sponsor!",
+        logoUrl: logoPlaceHolder.src,
+        url: "/sponsors/become-a-sponsor",
+        description:
+          "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      },
+    ],
+  },
+  {
+    sponsorLevelName: "Supporter",
+    sizeOnLargeScreen: 2,
+    sizeOnMediumScreen: 2,
+    sizeOnSmallScreen: 1,
+    showPopup: true,
+    list: [
+      {
+        name: "Become a sponsor!",
+        logoUrl: logoPlaceHolder.src,
+        url: "/sponsors/become-a-sponsor",
+        description:
+          "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      },
+    ],
+  },
+  {
+    sponsorLevelName: "Media Partner",
+    sizeOnLargeScreen: 2,
+    sizeOnMediumScreen: 2,
+    sizeOnSmallScreen: 1,
+    showPopup: true,
+    list: [
+      {
+        name: "Become a sponsor!",
+        logoUrl: annapurna.src,
+        url: "/sponsors/become-a-sponsor",
+        description:
+          "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      },
+    ],
+  },
+  {
+    sponsorLevelName: "Community Partners",
+    sizeOnLargeScreen: 2,
+    sizeOnMediumScreen: 2,
+    sizeOnSmallScreen: 1,
+    showPopup: false,
+    list: [
+      {
+        name: "Become a sponsor!",
+        logoUrl: logoPlaceHolder.src,
+        url: "/sponsors/become-a-sponsor",
+        description:
+          "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      },
+    ],
+  },
+];
