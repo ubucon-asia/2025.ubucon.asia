e8272e4752d974044dbcd4ca813fc372382288b6
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2025-04-30 08:49:32 +0545

    Add fiscal and community partner

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,7 +1,7 @@
-
 import logoPlaceHolder from "@assets/logo_placeholder.svg";
-import annapurna from "@assets/sponsors/annapurna.png"
-
+import annapurna from "@assets/sponsors/annapurna.png";
+import global_cyber from "@assets/sponsors/Global_Cybersecurity_Community.png";
+import impact_nexus from "@assets/sponsors/impact_nexus_nepal.png";
 
 export const sponsors = [
   {
@@ -101,6 +101,21 @@ export const sponsors = [
     ],
   },
   {
+    sponsorLevelName: "Fiscal Partner",
+    sizeOnLargeScreen: 2,
+    sizeOnMediumScreen: 2,
+    sizeOnSmallScreen: 1,
+    showPopup: false,
+    list: [
+      {
+        name: "Impact Nexus Nepal",
+        logoUrl: impact_nexus.src,
+        url: "",
+        description: "",
+      },
+    ],
+  },
+  {
     sponsorLevelName: "Community Partners",
     sizeOnLargeScreen: 2,
     sizeOnMediumScreen: 2,
@@ -108,11 +123,10 @@ export const sponsors = [
     showPopup: false,
     list: [
       {
-        name: "Become a sponsor!",
-        logoUrl: logoPlaceHolder.src,
-        url: "/sponsors/become-a-sponsor",
-        description:
-          "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+        name: "Global Cybersecurity Community Forum",
+        logoUrl: global_cyber.src,
+        url: "",
+        description: "",
       },
     ],
   },
