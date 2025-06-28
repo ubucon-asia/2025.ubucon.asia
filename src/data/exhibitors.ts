059245fd3531be9ae4fa2852ec2852cc240bb91c
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2025-06-28 11:19:26 +0545

    Add Sponsor booth

--- src/data/exhibitors.ts
+++ src/data/exhibitors.ts
@@ -3,30 +3,37 @@ import Canonical from "@assets/sponsors/Canonical_Ubuntu.svg";
 import DeepComputing from "@assets/sponsors/DeepComputing.svg";
 import Ubu4Cut from "@assets/exhibitors/ubu4cut.png"
 import UbuntuKylinOpenKylin from "@assets/exhibitors/ubuntukylin_openkylin.png";
+import ThingsCyber from "@assets/exhibitors/things_cyber.png";
 
 export const exhibitors = [
- {
+  {
     type: "Community Booth",
     title: "Ubu4Cut Booth",
     imgSrc: Ubu4Cut.src,
-    href: "https://events.canonical.com/event/127/contributions/681/"
- },
- {
+    href: "https://events.canonical.com/event/127/contributions/681/",
+  },
+  {
     type: "Community Booth",
     title: "Ubuntu Kylin & openKylin Booth",
     imgSrc: UbuntuKylinOpenKylin.src,
-    href: "https://events.canonical.com/event/127/contributions/682/"
- },
- {
+    href: "https://events.canonical.com/event/127/contributions/682/",
+  },
+  {
     type: "Sponsor Booth",
     title: "Canonical Booth",
     imgSrc: Canonical.src,
-    href: "https://events.canonical.com/event/127/contributions/684/"
- },
- {
+    href: "https://events.canonical.com/event/127/contributions/684/",
+  },
+  {
     type: "Sponsor Booth",
     title: "DeepComputing Booth",
     imgSrc: DeepComputing.src,
-    href: "https://events.canonical.com/event/127/contributions/683/"
- }
+    href: "https://events.canonical.com/event/127/contributions/683/",
+  },
+  {
+    type: "Sponsor Booth",
+    title: "Things Cyber Booth",
+    imgSrc: ThingsCyber.src,
+    href: "https://events.canonical.com/event/127/contributions/683/",
+  },
 ];
