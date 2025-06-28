e4cac972914c27f8a0b927657d9c3ac2729200f7
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2025-06-28 11:51:54 +0545

    Add gap between logo

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -26,38 +26,38 @@ export const sponsors = [
       },
     ],
   },
-  {
-    sponsorLevelName: "Gold",
-    sizeOnLargeScreen: 4,
-    sizeOnMediumScreen: 4,
-    sizeOnSmallScreen: 3,
-    showPopup: true,
-    list: [
-      // {
-      //   name: "Become a sponsor!",
-      //   logoUrl: logoPlaceHolder.src,
-      //   url: "/sponsors/become-a-sponsor",
-      //   description:
-      //     "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
-      // },
-    ],
-  },
-  {
-    sponsorLevelName: "Silver",
-    sizeOnLargeScreen: 3,
-    sizeOnMediumScreen: 3,
-    sizeOnSmallScreen: 2,
-    showPopup: true,
-    list: [
-      // {
-      //   name: "Become a sponsor!",
-      //   logoUrl: logoPlaceHolder.src,
-      //   url: "/sponsors/become-a-sponsor",
-      //   description:
-      //     "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
-      // },
-    ],
-  },
+  // {
+  //   sponsorLevelName: "Gold",
+  //   sizeOnLargeScreen: 4,
+  //   sizeOnMediumScreen: 4,
+  //   sizeOnSmallScreen: 3,
+  //   showPopup: true,
+  //   list: [
+  //     {
+  //       name: "Become a sponsor!",
+  //       logoUrl: logoPlaceHolder.src,
+  //       url: "/sponsors/become-a-sponsor",
+  //       description:
+  //         "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+  //     },
+  //   ],
+  // },
+  // {
+  //   sponsorLevelName: "Silver",
+  //   sizeOnLargeScreen: 3,
+  //   sizeOnMediumScreen: 3,
+  //   sizeOnSmallScreen: 2,
+  //   showPopup: true,
+  //   list: [
+  //     {
+  //       name: "Become a sponsor!",
+  //       logoUrl: logoPlaceHolder.src,
+  //       url: "/sponsors/become-a-sponsor",
+  //       description:
+  //         "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+  //     },
+  //   ],
+  // },
   {
     sponsorLevelName: "Bronze",
     sizeOnLargeScreen: 2,
@@ -83,22 +83,22 @@ export const sponsors = [
       }
     ],
   },
-  {
-    sponsorLevelName: "Supporter",
-    sizeOnLargeScreen: 2,
-    sizeOnMediumScreen: 2,
-    sizeOnSmallScreen: 1,
-    showPopup: true,
-    list: [
-      // {
-      //   name: "Become a sponsor!",
-      //   logoUrl: logoPlaceHolder.src,
-      //   url: "/sponsors/become-a-sponsor",
-      //   description:
-      //     "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
-      // },
-    ],
-  },
+  // {
+  //   sponsorLevelName: "Supporter",
+  //   sizeOnLargeScreen: 2,
+  //   sizeOnMediumScreen: 2,
+  //   sizeOnSmallScreen: 1,
+  //   showPopup: true,
+  //   list: [
+  //     {
+  //       name: "Become a sponsor!",
+  //       logoUrl: logoPlaceHolder.src,
+  //       url: "/sponsors/become-a-sponsor",
+  //       description:
+  //         "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+  //     },
+  //   ],
+  // },
   {
     sponsorLevelName: "Education Partner",
     sizeOnLargeScreen: 2,
