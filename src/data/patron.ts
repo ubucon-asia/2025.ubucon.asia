a827d2f154945fa4dd2d32025c0a8c41afac68bc
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2025-09-16 19:06:31 +0545

    Add patron

--- src/data/patron.ts
+++ src/data/patron.ts
@@ -1,7 +1,16 @@
 import sakushalImg from "../assets/patron/sukushal.jpeg";
-export const ticketPatron = {
-  name: "Sakushal Shrestha",
-  img: sakushalImg.src,
-  amount: "Rs. 10,000",
-  url: "https://www.linkedin.com/in/sakushalshrestha56/",
-};
+import sudipImg from "../assets/patron/sudip.jpeg";
+export const ticketPatron = [
+  {
+    name: "Sakushal Shrestha",
+    img: sakushalImg.src,
+    amount: "Rs. 10,000",
+    url: "https://www.linkedin.com/in/sakushalshrestha56/",
+  },
+  {
+    name: "Sudip Limbu",
+    img: sudipImg.src,
+    amount: "Rs. 10,000",
+    url: "https://www.linkedin.com/in/4slimbu/",
+  },
+];
